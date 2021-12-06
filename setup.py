cc4f850c1f170c282a243456cfae129406d8fdaa
AzizbekDeveloper <73469043+azizbekQozoqov@users.noreply.github.com>
2021-12-06 18:51:12 +0300

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='AzizbekDeveloper',
+      url='https://www.azizbekdeveloper.herokuapp.com',
+      packages=['instaloader'],
+)
